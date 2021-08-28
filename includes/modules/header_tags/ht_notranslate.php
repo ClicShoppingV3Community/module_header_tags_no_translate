6f9b5db91fb66092868eafa1cbdf9f5936c63489
ClicShopping <admin@clicshopping.org>
2021-08-28 14:43:43 +0200

    ADD TYPAGE

--- includes/modules/header_tags/ht_notranslate.php
+++ includes/modules/header_tags/ht_notranslate.php
@@ -14,10 +14,10 @@
 
   class ht_notranslate
   {
-    public $code;
+    public string $code;
     public $group;
-    public string $title;
-    public string $description;
+    public $title;
+    public $description;
     public ?int $sort_order = 0;
     public bool $enabled = false;
     public array $languages_array = [];
